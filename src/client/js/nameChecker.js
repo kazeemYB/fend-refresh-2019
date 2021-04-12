cca9b7b7aaa97ee2d35126f45eea0c83f0523d74
yomex <yomexbada@gmail.com>
2021-04-13 00:29:00 +0100

    This commit will show fixes to _test_ with nameChecker for Jest to pass all tests

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,18 +1,8 @@
-const checkForName = (formText) => {
-    var res = formText.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    var url = document.querySelector('#url').value;
-
-    if (res == null) {
-        alert("Please enter a valid URL");
-        console.log(`Invalid URL --> ${url}`);
-        document.querySelector('#invalid').innerHTML = `Please Enter a Valid URL - (${url})`;
-        return false;
-    } else {
-        document.querySelector('#invalid').innerHTML = "";
-        console.log(`Valid URL --> ${url}`);
-        return true;
-    }
+function checkForName(formText) {
+    const url = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    return url.test(formText);
 }
+
 
 
 export { checkForName }
