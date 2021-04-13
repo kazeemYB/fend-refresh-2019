3d62a0a6ce8e7bbbf212e1267d73f1cb6aaa28b9
yomex <yomexbada@gmail.com>
2021-04-13 21:45:26 +0100

    This commit will add RegExp to nameChecker for Jest to pass all tests

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,7 +1,7 @@
 function checkForName(formText) {
     const url = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
     // return url.test(formText);
-    var regex = 'pattern';
+    var regex = url;
     var newUrl = new RegExp(regex, 'i');
     return newUrl.test(formText);
 }
